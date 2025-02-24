92870240fb2ab2ba26e5397f3bc4ce73253b50e8
Rashmi-Juli <rashmijuliyange@gmail.com>
2025-02-24 21:29:39 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+  double length=25.12428;
+  double width=12.59;
+  double area=length*width;
+  printf("Area of the rectangle:%.4fcm^2\n",area);
+
+  return 0;
+  }
---
+++ lmx.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+  printf("The Reaverse of XML is %c %c %c\n",'L','M','X');
+  return 0;
+  }
